65e9ec77d9f899db1a286d3e0900c33a59b4d4bb
tandrn <danqtut2@gmail.com>
2025-10-13 15:48:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+BOT_TOKEN=7759070238:AAHyBInKYBJwvVznTQzfveJyock7XsJpSlA
 MINI_APP_URL = os.getenv(
     "MINI_APP_URL", "https://tandrn.github.io/find-yourself-mini-app/")
