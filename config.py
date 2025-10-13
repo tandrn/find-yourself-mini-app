781b263d8475824dc07ddacb5c3dec9eb4f2d249
tandrn <danqtut2@gmail.com>
2025-10-13 15:28:37 +0300

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-MINI_APP_URL = os.getenv("MINI_APP_URL", "https://tandrn.github.io/find-yourself-bot")
